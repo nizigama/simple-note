12780f8f13f619b0a33a5266d9cdddf55b4856c9
Nizigama jean davy <jedavy.n@gmail.com>
2021-04-22 20:01:34 +0200

    Implemented the log feature and added the index handler to the root path

--- main.go
+++ main.go
@@ -7,37 +7,35 @@ import (
 	"os"
 )
 
-func init() {
-	logFile, err := os.Open("app-log")
+var logger *log.Logger
+
+func main() {
+
+	logFile, err := os.OpenFile("app-log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
 
 	if err != nil {
-		logFile, err = os.Create("app-log")
-		if err != nil {
-			fmt.Println("Failed to create the log file")
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		fmt.Println("Failed to create the log file")
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	defer logFile.Close()
 
-	log.SetOutput(logFile)
-	log.SetPrefix("simple-note-app:")
-}
-
-func main() {
+	logger = log.New(logFile, "simple-note-app:", log.Ldate)
 
 	defer handlePanic()
 
-	err := http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", index)
+
+	err = http.ListenAndServe(":3000", nil)
 
 	if err != nil {
-		log.Fatalln("Error starting the server, here is the error:", err)
+		logger.Fatalln("Error starting the server, here is the error:", err)
 	}
 }
 
 func handlePanic() {
 	if message := recover(); message != nil {
-		log.Fatalln("Application paniced, here is the message:", message)
+		logger.Fatalln("Panic occurred:", message)
 	}
 }
