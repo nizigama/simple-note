06d1368bf3801982ba0a5dac5e07f890a91b2a10
Nizigama jean davy <jedavy.n@gmail.com>
2021-04-24 12:14:30 +0200

    FInished working on the registration handler
    Next is working on the database support

--- handlers.go
+++ handlers.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 	"text/template"
 	"time"
+
+	users "github.com/nizigama/simple-note/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var tpl *template.Template
@@ -23,6 +28,96 @@ func login(w http.ResponseWriter, req *http.Request) {
 }
 
 func register(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	tpl.ExecuteTemplate(w, "register.html", time.Now().Year())
+
+	if req.Method == http.MethodPost {
+
+		if err := req.ParseForm(); err != nil {
+			http.Error(w, "Error parsing your request", http.StatusUnprocessableEntity)
+		}
+
+		firstName, lastName, email, password, confirm := req.Form.Get("first"), req.Form.Get("last"), req.Form.Get("email"), req.Form.Get("pass"), req.Form.Get("confirm")
+
+		if strings.Trim(firstName, " ") == "" {
+			http.Error(w, "First name is required", http.StatusUnprocessableEntity)
+			return
+		}
+		if strings.Trim(lastName, " ") == "" {
+			http.Error(w, "Last name is required", http.StatusUnprocessableEntity)
+			return
+		}
+		if strings.Trim(email, " ") == "" {
+			http.Error(w, "Email is required", http.StatusUnprocessableEntity)
+			return
+		}
+		if isValid := validateEmail(email); isValid != nil {
+			http.Error(w, "invalid email address", http.StatusUnprocessableEntity)
+			return
+		}
+		if strings.Trim(password, " ") == "" {
+			http.Error(w, "Password is required", http.StatusUnprocessableEntity)
+			return
+		}
+		if strings.Trim(confirm, " ") == "" {
+			http.Error(w, "Password confirmation is required", http.StatusUnprocessableEntity)
+			return
+		}
+		if password != confirm {
+			http.Error(w, "Passwords don't match", http.StatusUnprocessableEntity)
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MaxCost)
+
+		if err != nil {
+			http.Error(w, "Password hashing failed, Contact support", http.StatusInternalServerError)
+			return
+		}
+
+		newUser := users.User{
+			FirstName:  firstName,
+			LastName:   lastName,
+			Email:      email,
+			Password:   hashedPassword,
+			RememberMe: true,
+		}
+
+		if err := newUser.Save(); err != nil {
+			http.Error(w, "Signup failed, Contact support", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Location", "/profile")
+		w.WriteHeader(http.StatusOK)
+
+	} else {
+		w.Header().Set("Content-Type", "text/html")
+		tpl.ExecuteTemplate(w, "register.html", time.Now().Year())
+	}
+}
+
+func validateEmail(email string) error {
+
+	if !strings.Contains(email, "@") || !strings.Contains(email, ".") {
+		return fmt.Errorf("invalid email")
+	}
+
+	parts := strings.Split(email, "@")
+
+	// if there is no content before or after the @ symbol
+	if len(parts[0]) == 0 || len(parts[1]) == 0 {
+		return fmt.Errorf("invalid email")
+	}
+
+	afterAtSymbol := strings.Split(parts[1], ".")
+	// if there is a dot after the @ symbol
+	if len(afterAtSymbol) == 0 {
+		return fmt.Errorf("invalid email")
+	}
+
+	// if there is content after the last dot(.)
+	if len(afterAtSymbol[len(afterAtSymbol)-1]) == 0 {
+		return fmt.Errorf("invalid email")
+	}
+
+	return nil
 }
