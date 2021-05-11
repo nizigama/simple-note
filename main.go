8054a2854da46f9c3f9ecdf9e02bae93281d8a71
Nizigama jean davy <jedavy.n@gmail.com>
2021-05-11 19:56:57 +0200

    Implemented gorilla mux router on all routes

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/nizigama/simple-note/models"
 	boltDB "github.com/nizigama/simple-note/services/database"
 	auth "github.com/nizigama/simple-note/services/middleware"
@@ -37,18 +38,22 @@ func main() {
 	defer logFile.Close()
 	defer handlePanic()
 
-	http.HandleFunc("/", index)
-	http.Handle("/login", auth.Authorize(login))
-	http.Handle("/register", auth.Authorize(register))
-	http.Handle("/profile", auth.Authorize(profile))
-	http.Handle("/profile-picture", auth.Authorize(profilePicture))
-	http.HandleFunc("/get-picture", getPicture)
-	http.Handle("/dashboard", auth.Authorize(dashboard))
-	http.Handle("/new-note", auth.Authorize(newNote))
-	http.Handle("/update-note", auth.Authorize(updateNote))
-	http.Handle("/delete-note", auth.Authorize(deleteNote))
-	http.Handle("/logout", auth.Authorize(logout))
-	http.Handle("/delete-account", auth.Authorize(deleteAccount))
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", index).Methods("GET")
+	r.Handle("/login", auth.Authorize(login))
+	r.Handle("/register", auth.Authorize(register))
+	r.Handle("/profile", auth.Authorize(profile))
+	r.Handle("/profile-picture", auth.Authorize(profilePicture)).Methods("GET")
+	r.HandleFunc("/get-picture", getPicture).Methods("GET")
+	r.Handle("/dashboard", auth.Authorize(dashboard)).Methods("GET")
+	r.Handle("/new-note", auth.Authorize(newNote))
+	r.Handle("/update-note", auth.Authorize(updateNote))
+	r.Handle("/delete-note", auth.Authorize(deleteNote)).Methods("GET")
+	r.Handle("/logout", auth.Authorize(logout)).Methods("GET")
+	r.Handle("/delete-account", auth.Authorize(deleteAccount)).Methods("GET")
+
+	http.Handle("/", r)
 
 	err := http.ListenAndServe(":3000", nil)
 
